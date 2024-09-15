e8ff7a93d12343a7a964400a980a67dee734bb26
Nabin Yari (Atit) <nabinyarijr@gmail.com>
2024-09-15 21:08:27 +0545

    Step - 3 making game more intresting by asking user money he/she want to spend.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Scanner;
 import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,7 +10,14 @@ public class App {
        System.out.println("          2- If you get one 7 number then 3 euro will be added to your balance.");
        System.out.println("          3- If you get two 7 number then 5 euro will be added to your balance.");
        System.out.println("          4- If you get three 7 number then 10 euro will be added to your balance.");
-       int money = 5;
+       System.out.println(" ");
+       System.out.println("How much money does you want to spend?");
+       int money = scr.nextInt();
+       System.out.println(" ");
+       System.out.println(" ");
+       //int money = 5;
+       Scanner scr2 = new Scanner(System.in);
+       String userInput;
        do{
         int ranNum1 = random.nextInt(10)+1;
         System.out.println( ranNum1);
@@ -21,7 +29,6 @@ public class App {
         {
             System.out.println("You won!");
             money = (money-1) + 10;
-
         }
         else if(ranNum1 == 7 && ranNum2 == 7)
         {
@@ -38,6 +45,8 @@ public class App {
             System.out.println("You lost!");
             money--;
         }
-       }while(money != 0);
+        System.out.println("Do you want to play again if yes press 'yes' or 'no' for not to play:");
+        userInput = scr2.nextLine();
+       }while(!userInput.equals("no"));
     }
 }
