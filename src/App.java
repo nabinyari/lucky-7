d294168ce4af2a74ea120272d52890a6169b2718
Nabin Yari (Atit) <nabinyarijr@gmail.com>
2024-09-15 20:38:14 +0545

    added money features to make game more intresting (step-2)

--- src/App.java
+++ src/App.java
@@ -4,20 +4,40 @@ public class App {
     public static void main(String[] args) throws Exception {
        Scanner scr = new Scanner(System.in);
        Random random = new Random();
-       for(int i= 0; i<3; i++)
-       {
-        int randNum = random.nextInt(10)+1;
-        System.out.println(randNum);
-        if(randNum == 7)
+       System.out.println("*****Note:");
+       System.out.println("          1- You have 5 euro for this game.");
+       System.out.println("          2- If you get one 7 number then 3 euro will be added to your balance.");
+       System.out.println("          3- If you get two 7 number then 5 euro will be added to your balance.");
+       System.out.println("          4- If you get three 7 number then 10 euro will be added to your balance.");
+       int money = 5;
+       do{
+        int ranNum1 = random.nextInt(10)+1;
+        System.out.println( ranNum1);
+        int ranNum2 = random.nextInt(10)+1;
+        System.out.println( ranNum2);
+        int ranNum3 = random.nextInt(10)+1;
+        System.out.println( ranNum3);
+        if(ranNum1 == 7 && ranNum2 == 7 && ranNum3 == 7)
         {
-            System.out.println("You won.");
+            System.out.println("You won!");
+            money = (money-1) + 10;
+
         }
+        else if(ranNum1 == 7 && ranNum2 == 7)
+        {
+           System.out.println("You won!");
+           money = (money-1) + 5;
+        }
+        else if(ranNum1 == 7 || ranNum2 == 7 || ranNum3 == 7 )
+            {
+                System.out.println("You won!");
+                money = (money-1) + 3;
+            }
         else
         {
-            System.out.println("You lost.");
+            System.out.println("You lost!");
+            money--;
         }
-       }
-
+       }while(money != 0);
     }
 }
- 
